e96a7598990b1227241cf49c3f0406b746067428
Juanchos2905 <juanchos2905@gmail.com>
2023-09-16 17:52:32 -0500

    Class Date with exceptions done

--- EmployeeOOP/Program.cs
+++ EmployeeOOP/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmployeeOOP.Classes;
+
+try
+{
+    //Declaración de variables
+    int day, month, year;
+
+    Console.WriteLine("OOP APPLICATION");
+    Console.WriteLine("---------------");
+
+    Console.Write("Ingresar el día: ");
+    day = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el mes: ");
+    month = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el año: ");
+    year = Convert.ToInt32(Console.ReadLine());
+
+    //Inicialización de la clase Date
+    Date dateObject = new Date(day, month, year);
+    Console.WriteLine("\n");
+    Console.WriteLine(dateObject.ToString());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
